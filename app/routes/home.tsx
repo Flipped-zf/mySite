f8f216f8d402a595f2a5265e60ed25fddacae470
tommy <468369392@qq.com>
2025-02-28 18:30:11 +0800

    bug: 3d-pin.tsx中a标签嵌套

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,7 +2,7 @@
  * @Author: tommy 468369392@qq.com
  * @Date: 2025-02-27 09:51:47
  * @LastEditors: tommy 468369392@qq.com
- * @LastEditTime: 2025-02-28 15:11:30
+ * @LastEditTime: 2025-02-28 18:24:22
  * @FilePath: \my-site\app\routes\home.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -14,6 +14,8 @@ import { MeteorDemo } from '~/magicui/MeteorDemo'
 import { OrbitingCirclesDemo } from '~/magicui/OrbitingCirclesDemo'
 import { TiltedCardDemo } from '~/reactbits/TiltedCardDemo'
 import { HyperspeedDemo } from '~/reactbits/HyperspeedDemo'
+import { ThreeDCardDemo } from '~/aceternity/ThreeDCardDemo'
+import { AnimatedPinDemo } from '~/aceternity/AnimatedPinDemo'
 export function meta({}: Route.MetaArgs) {
   return [
     { title: 'New React Router App' },
@@ -39,6 +41,10 @@ export default function Home() {
       <div className="relative w-full">
         <TiltedCardDemo></TiltedCardDemo>
         <HyperspeedDemo></HyperspeedDemo>
+      </div>
+      <div className="relative w-full">
+        <ThreeDCardDemo></ThreeDCardDemo>
+        <AnimatedPinDemo></AnimatedPinDemo>
       </div>
     </div>
   )
